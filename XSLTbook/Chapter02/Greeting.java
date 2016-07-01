88f06b2096767b5d48c50c0140db67bea7bb0b76
Krakl <slayerofthekraken@gmail.com>
2016-07-01 14:13:06 -0700

    XSLT training

---
+++ XSLTbook/Chapter02/Greeting.java
@@ -0,0 +1,9 @@
+
+            public class Greeting
+            {
+                public static void main(String[] argv)
+                {
+                System.out.println("Hello, my name is Tom Riddle. How did you come by my Unix shell?");
+                }
+             }
+         
