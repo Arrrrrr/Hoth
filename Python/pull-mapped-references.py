4270cc6e4ec44d8c2e4f2cd8ce5004b44fefa037
Arrrrrr <slayerofthekraken@gmail.co]m>
2017-08-01 13:26:50 -0700

    comment added

--- Python/pull-mapped-references.py
+++ Python/pull-mapped-references.py
@@ -10,6 +10,7 @@ xmlfilepath = "source_xml/my_file.xml"
 
 main_xmlfile = open(xmlfilepath)
 for line in main_xmlfile:
+	# first, you break the line into groups. then, you pick which group to print.
     d = re.match(r"(.*href=\")(.*\.xml)", str(line))
     if d: 
         clean_xmlfile = d.group(2)
