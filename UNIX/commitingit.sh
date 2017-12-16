49a01ce2de979c6319ce6999417d28b6e134ff99
Arrrrrr <slayerofthekraken@gmail.com>
2017-12-15 16:43:41 -0800

    new scriptsies

---
+++ UNIX/commitingit.sh
@@ -0,0 +1,9 @@
+# replace directory and Message
+
+cd directory
+git add .
+git commit -a -m "Message"
+git push
+git pull
+
+cd ..
--- UNIX/replacerabbit.sh
+++ UNIX/replacerabbit.sh
@@ -1,4 +1,4 @@
-replace word 'rabbit' with word 'wabbit' in all xml files
+# replace word 'rabbit' with word 'wabbit' in all xml files
 
 find . -name '*xml' -exec bash -c ' mv $0 ${0/\rabbit/wabbit}' {} \;
 
