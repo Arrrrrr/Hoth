5dc92d979b33864570313e25bffe62703ee76067
Arrrrrr <slayerofthekraken@gmail.com>
2018-05-31 09:25:32 -0700

    Make Commitment Average Again (MCAA)

--- UNIX/commitingit.sh
+++ UNIX/commitingit.sh
@@ -1,14 +1,53 @@
-echo Hi! I am your buddy Spuddy, wanna commit stuff?
-read uhyeah
-echo Sweet! What is your commit message?
-read tacos
-echo Your message was: $tacos
+echo Hi there! My name is Helga. Would you like to commit stuff? \(y/n\)?
+read reply
+echo Your message was: $reply
+if [ "$reply" == "y" ]; then
+	echo "OK, committing to your repo"
+	echo What is your commit message?
+	read yourmessage2
+	echo Your message was: $yourmessage2
+	echo committing to your repo
+	# add path to your repo below
+	cd repo
+	git pull
+	git add .
+	git commit -a -m "$yourmessage2"
+	git pull
+	git push
+	git pull
+	echo committing to your repo
+elif [ "$reply" == "yes helga" ]; then
+	echo "Have some bacon, committing to your repo"
+	echo What is your commit message?
+	read yourmessage2
+	echo Your message was: $yourmessage2
+	echo committing to your repo
+	# add path to your repo below
+	cd repo
+	git pull
+	git add .
+	git commit -a -m "$yourmessage2"
+	git pull
+	git push
+	git pull
+	echo commit and push is complete
+elif [ "$reply" == "yes" ]; then
+	echo "OK, committing to your repo"
+	echo What is your commit message?
+	read yourmessage2
+	echo Your message was: $yourmessage2
+	echo committing to your repo
+	# add path to your repo below
+	cd repo
+	git pull
+	git add .
+	git commit -a -m "$yourmessage2"
+	git pull
+	git push
+	git pull
+	echo commit and push is complete
+else
+	echo "OK, I can see you are not ready for commitment. It's cool."
+fi
 
-
-cd directory
-git add .
-git commit -a -m "$tacos"
-git push
-git pull
-
-cd ..
+echo My work here is done. Enjoy the bacon.
